3b5ea833a1dacc425de3c8af2852eaf5a2775770
sheyneee <sheynedelacruz@gmail.com>
2024-09-11 22:24:29 +0800

    Fixed database location

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb+srv://iservebarangay:agIB3Xn6pUhwmPyY@isb-server.3jomc.mongodb.net/?retryWrites=true&w=majority&appName=isb-server')
     .then(() => console.log('>> Established a connection to the database <<'))
     .catch(err => console.log('>> Something went wrong when connecting to the database <<', err));  
