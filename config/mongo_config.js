de8dff0b803b162b929f400b90a4260fde2632cc
sheyneee <sheynedelacruz@gmail.com>
2024-09-17 23:23:00 +0800

    updated mongo_config

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://iservebarangay:agIB3Xn6pUhwmPyY@isb-server.3jomc.mongodb.net/?retryWrites=true&w=majority&appName=isb-server')
+mongoose.connect('mongodb+srv://iservebarangay:agIB3Xn6pUhwmPyY@isb-server.3jomc.mongodb.net/iservebarangay?retryWrites=true&w=majority&appName=isb-server')
     .then(() => console.log('>> Established a connection to the database <<'))
     .catch(err => console.log('>> Something went wrong when connecting to the database <<', err));  
-    
