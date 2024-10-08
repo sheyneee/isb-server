0209401f78872a4101e48c4546bfd98bdedbe865
sheyneee <sheynedelacruz@gmail.com>
2024-10-09 00:06:36 +0800

    fixed routes

--- routes/barangay/hotlinesRoutes.js
+++ routes/barangay/hotlinesRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const hotlinesController = require('../../controllers/barangay/hotlinesController');
 
 router.get('/all/hotlines', hotlinesController.upload, hotlinesController.getAllHotlines);
-router.post('/new/barangay', hotlinesController.createHotline); 
-router.get('/barangay/:id', hotlinesController.getHotlineById);
-router.put('/barangay/:id', hotlinesController.updateHotline);
-router.delete('/barangay/:id', hotlinesController.deleteHotline);
+router.post('/new/hotlines', hotlinesController.createHotline); 
+router.get('/hotlines/:id', hotlinesController.getHotlineById);
+router.put('/hotlines/:id', hotlinesController.updateHotline);
+router.delete('/hotlines/:id', hotlinesController.deleteHotline);
 
 module.exports = router;
